2d375cf25b6cb9355fcc4d5296d5337bf9b6e2e6
Ian McCormack <icmccorm@andrew.cmu.edu>
2025-03-23 22:24:28 +0000

    Added back link to host sysroot for platforms that use dynamic libraries for sanitizers.

--- src/tools/bsan/bsan-driver/src/lib.rs
+++ src/tools/bsan/bsan-driver/src/lib.rs
@@ -3,8 +3,8 @@
 
 extern crate rustc_driver;
 
+use std::env;
 use std::sync::Arc;
-
 pub const BSAN_BUG_REPORT_URL: &str = "https://github.com/BorrowSanitizer/rust/issues/new";
 
 pub const BSAN_DEFAULT_ARGS: &[&str] =
@@ -21,7 +21,11 @@ pub fn run_compiler(
     using_internal_features: Arc<std::sync::atomic::AtomicBool>,
 ) -> ! {
     if target_crate {
-        let additional_args = BSAN_DEFAULT_ARGS.iter().map(ToString::to_string).collect::<Vec<_>>();
+        let mut additional_args =
+            BSAN_DEFAULT_ARGS.iter().map(ToString::to_string).collect::<Vec<_>>();
+        if let Some(runtime) = env::var_os("BSAN_HOST_SYSROOT") {
+            additional_args.push(format!("-L{}/lib", runtime.to_string_lossy()));
+        }
         args.splice(1..1, additional_args);
     }
     let exit_code = rustc_driver::catch_with_exit_code(move || {
