82c97dd03a0855b4bfdb7adedc48c522461ea6aa
Ian McCormack <icmccorm@cs.cmu.edu>
2025-05-19 18:38:38 -0400

    Added back cbindgen.

--- src/tools/bsan/bsan-rt/build.rs
+++ src/tools/bsan/bsan-rt/build.rs
@@ -1,8 +1,7 @@
-//use std::env;
-//use std::path::Path;
+use std::env;
+use std::path::Path;
 
 fn main() {
-    /*
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_dir = env::var("BSAN_HEADER_DIR").unwrap_or(env::var("OUT_DIR").unwrap());
     cbindgen::Builder::new()
@@ -10,5 +9,5 @@ fn main() {
         .with_crate(crate_dir.clone())
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file(Path::new(&out_dir).join("bsan_rt.h"));*/
+        .write_to_file(Path::new(&out_dir).join("bsan_rt.h"));
 }
